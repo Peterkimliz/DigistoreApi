a2fcb378cbc356bb4ca19a7a080aaff7cbc0b6a5
PeterKimLiz <githumbi3fred@email.com>
2023-01-03 14:55:43 +0300

    cleaned up shop controller

--- src/main/java/com/example/digishopapi/controllers/ShopController.java
+++ src/main/java/com/example/digishopapi/controllers/ShopController.java
@@ -6,7 +6,6 @@ import com.example.digishopapi.dtos.UpdateShopDto;
 import com.example.digishopapi.models.Shop;
 import com.example.digishopapi.services.ShopService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -23,24 +22,24 @@ public class ShopController {
 
     @PostMapping("/create/{userId}")
     public ResponseEntity<Shop> createShop( @PathVariable("userId") String userId, @RequestBody @Validated ShopDto shopDto) {
-        return new ResponseEntity<Shop>(shopService.createShop(userId,shopDto), HttpStatus.CREATED);
+        return new ResponseEntity<>(shopService.createShop(userId, shopDto), HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Shop> getShopById(@PathVariable("id") String id) {
-        return new ResponseEntity<Shop>(shopService.getShopById(id), HttpStatus.OK);
+        return new ResponseEntity<>(shopService.getShopById(id), HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Shop> updateShopById(@PathVariable("id") String id, @RequestBody UpdateShopDto updateShopDto) {
-        return new ResponseEntity<Shop>(shopService.updateShopById(id, updateShopDto), HttpStatus.OK
+        return new ResponseEntity<>(shopService.updateShopById(id, updateShopDto), HttpStatus.OK
         );
     }
 
     @GetMapping("/all")
     public ResponseEntity<List<Shop>> getShops(@RequestParam (required = false) String pageNumber) {
         List<Shop> shops = shopService.getAllShops(pageNumber);
-        return new ResponseEntity<List<Shop>>(shops, shops.size() == 0 ? HttpStatus.NOT_FOUND : HttpStatus.OK);
+        return new ResponseEntity<>(shops, shops.size() == 0 ? HttpStatus.NOT_FOUND : HttpStatus.OK);
     }
 
 
