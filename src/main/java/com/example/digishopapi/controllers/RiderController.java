377d30b8765df4c10995f21a626793ab2f5d436b
PeterKimLiz <githumbi3fred@email.com>
2023-01-04 12:49:17 +0300

    finished riders api

--- src/main/java/com/example/digishopapi/controllers/RiderController.java
+++ src/main/java/com/example/digishopapi/controllers/RiderController.java
@@ -9,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/riders")
 public class RiderController {
@@ -24,10 +26,24 @@ public class RiderController {
     public ResponseEntity<Rider> getRiderById(@PathVariable("riderId") String riderId) {
         return new ResponseEntity<>(riderService.getRiderById(riderId), HttpStatus.OK);
     }
+    @GetMapping("/shop/{shopId}")
+    public ResponseEntity<List<Rider>> getRidersByShopId(@PathVariable("shopId") String shopId) {
+        List<Rider> riders=  riderService.getRidersByShopId(shopId);
+        return new ResponseEntity<>(riders,riders.size()==0?HttpStatus.NOT_FOUND: HttpStatus.OK);
+    } @GetMapping("/all")
+    public ResponseEntity<List<Rider>> getAllRiders(@RequestParam(required = false) String pageNumber) {
+        List<Rider> riders= riderService.getAllRiders(pageNumber);
+        return new ResponseEntity<>(riders,riders.size()==0?HttpStatus.NOT_FOUND: HttpStatus.OK);
+    }
+    @PutMapping("/{riderId}")
+    public ResponseEntity<Rider> updateRiderById(@PathVariable("riderId") String riderId,@RequestBody Rider rider) {
+        return new ResponseEntity<>(riderService.updateRiderById(riderId,rider), HttpStatus.OK);
+    }
     @DeleteMapping("/{riderId}")
     public ResponseEntity<?> deleteRiderById(@PathVariable("riderId") String riderId) {
         riderService.deleteRiderById(riderId);
         return new ResponseEntity<>("rider deleted successfull",HttpStatus.OK);
     }
+
 
 }
