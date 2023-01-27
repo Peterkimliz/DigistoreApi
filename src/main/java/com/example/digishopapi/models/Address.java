d20df9b9f69c958abcbc7846f8b8b1bb2736d3ee
Freddy <githumbi3fred@gmail.com>
2023-01-27 17:44:04 +0300

    added address model

---
+++ src/main/java/com/example/digishopapi/models/Address.java
@@ -0,0 +1,23 @@
+package com.example.digishopapi.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "address")
+public class Address {
+    @Id
+    private String id;
+    private String name;
+    private  String country;
+   private String state;
+   private String city;
+   private String phone;
+   private String userId;
+
+}
